9fa1b2e73a58ffedf040f35f6295c9cbeb22879e
IgorV <viazminiv@gmail.com>
2021-01-19 16:21:14 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,30 @@
 
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
 const cleancss = require('gulp-clean-css');
+const debug = require('gulp-debug');
 const gulp = require('gulp');
+const gulpif = require('gulp-if');
 const imagemin = require('gulp-imagemin');
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 const newer = require('gulp-newer');
 const sass = require('gulp-sass');
 const svgSprite = require('gulp-svg-sprite');
 const sourcemaps = require('gulp-sourcemaps');
-const debug = require('gulp-debug');
-const clean = require('gulp-clean');
 
 
 // https://www.npmjs.com/package/gulp-sourcemaps	++++
 // https://www.npmjs.com/package/gulp-clean			++++
-// https://node-swig.github.io/swig-templates/
-// https://www.npmjs.com/package/gulp-swig
-// https://www.npmjs.com/package/gulp-if
+// https://node-swig.github.io/swig-templates/		
+// https://www.npmjs.com/package/gulp-swig			/неактуально
+// https://www.npmjs.com/package/gulp-if			++++
 // https://www.npmjs.com/package/gulp-debug			++++
-// https://www.npmjs.com/package/gulp-imagemin
-
+// https://www.npmjs.com/package/gulp-imagemin		
 
 const { src, dest, parallel, series, watch } = gulp;
 
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 function startwatch  () {
 	gulp.watch('app/sass/*.scss', gulp.series('styles'));
@@ -39,12 +41,12 @@ gulp.task('browsersync', function(){
 
 gulp.task('styles' , function() {
 	return src('app/sass/main.scss')
-		.pipe(sourcemaps.init())
+		.pipe(gulpif(isDev, sourcemaps.init()))
 		.pipe(sass()) 
 		.pipe(debug({title: 'sass'}))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) 
 		.pipe(cleancss( { level: { 1: { specialComments: 0 } } } ))
-		.pipe(sourcemaps.write())
+		.pipe(gulpif(isDev, sourcemaps.write()))
 		.pipe(debug({title: 'sourcemap'}))
 		.pipe(dest('app/css/')) 
 		.pipe(browserSync.stream())
@@ -57,8 +59,26 @@ gulp.task('images', function(){
 		.pipe(dest('app/images/dest/')) // Выгружаем оптимизированные изображения в папку назначения
 });
 
+gulp.task('imageMin', function(){
+	return src('app/images/src2/**/*') 
+		.pipe(imagemin())
+		.pipe(dest('app/images/dest2/')) 
+});
+
+
+gulp.task('imageMin2', function(){
+	return src('app/images/src2/**/*') 
+		.pipe(imagemin([
+			imageminJpegRecompress()
+		]))
+		.pipe(dest('app/images/dest3/')) 
+});
+
+
+
+
 gulp.task('sSprite', function(){
-	return src('app/images/dest/*.svg') // svg files for sprite
+	return src('app/images/src/*.svg') // svg files for sprite
 		.pipe(svgSprite({
 			mode: {
 				stack: {
@@ -72,7 +92,7 @@ gulp.task('sSprite', function(){
 
 
 gulp.task('clean-svg', function(){
-	return src('app/images/src/*.svg', {read: false})
+	return src('app/images/dest/*.svg', {read: false})
 		.pipe(clean({force: true}))
 		.pipe(debug({title: 'clean-svg'}))
 });
